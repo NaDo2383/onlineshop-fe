7dd009f413876975d34f8cf839d4f9f0e863a855
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-02-02 09:02:09 +0800

    20230202 home

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        Logo
+                    </a>
+                    <span>Log Out</span>
+                </div>
+            </nav>
+        </div>
+    );
+}
---
+++ src/pages/admin.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../components/navbar";
+
+export default function Admin() {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+}
