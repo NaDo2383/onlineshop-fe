a8525bbae803dfabc9b7e11921ea2bb5fbe6c1bb
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-02-06 08:43:22 +0800

    20230206 home

--- src/components/addMenuModal.js
+++ src/components/addMenuModal.js
@@ -1,57 +1,74 @@
 import React from "react";
 
 export default function AddMenuModal({
-  modal,
-  setModal,
-  addingMenu,
-  setAddingMenu,
-  addMenu,
+    modal,
+    setModal,
+    addingMenu,
+    setAddingMenu,
+    addMenu,
 }) {
-  return (
-    <div
-      style={modal ? { display: "block" } : { display: "none" }}
-      id="addMenuModal"
-    >
-      <input
-        class="form-control addMenuModalInput"
-        type="text"
-        placeholder="Menu name"
-        aria-label="default input example"
-        value={addingMenu.menuName}
-        onChange={(e) =>
-          setAddingMenu({ ...addingMenu, menuName: e.target.value })
-        }
-      />
-      <input
-        class="form-control addMenuModalInput"
-        type="text"
-        placeholder="Menu link /example"
-        aria-label="default input example"
-        value={addingMenu.link}
-        onChange={(e) => setAddingMenu({ ...addingMenu, link: e.target.value })}
-      />
-      <input
-        class="form-control addMenuModalInput"
-        type="number"
-        placeholder="Position"
-        aria-label="default input example"
-        value={addingMenu.position}
-        onChange={(e) =>
-          setAddingMenu({ ...addingMenu, position: e.target.value })
-        }
-      />
-      <div className="addMenuModalBtns">
-        <button type="button" class="btn btn-success" onClick={() => addMenu()}>
-          Save
-        </button>
-        <button
-          type="button"
-          class="btn btn-secondary"
-          onClick={() => setModal(!modal)}
+    return (
+        <div
+            style={modal ? { display: "block" } : { display: "none" }}
+            id="addMenuModal"
         >
-          Close
-        </button>
-      </div>
-    </div>
-  );
+            <input
+                class="form-control addMenuModalInput"
+                type="text"
+                placeholder="Menu name"
+                aria-label="default input example"
+                style={{ display: "none" }}
+                value={addingMenu.id}
+                onChange={(e) =>
+                    setAddingMenu({ ...addingMenu, id: e.target.value })
+                }
+            />
+            <input
+                class="form-control addMenuModalInput"
+                type="text"
+                placeholder="Menu name"
+                aria-label="default input example"
+                value={addingMenu.menuName}
+                onChange={(e) =>
+                    setAddingMenu({ ...addingMenu, menuName: e.target.value })
+                }
+            />
+            <input
+                class="form-control addMenuModalInput"
+                type="text"
+                placeholder="Menu link /example"
+                aria-label="default input example"
+                value={addingMenu.link}
+                onChange={(e) =>
+                    setAddingMenu({ ...addingMenu, link: e.target.value })
+                }
+            />
+            <input
+                class="form-control addMenuModalInput"
+                type="number"
+                placeholder="Position"
+                aria-label="default input example"
+                value={addingMenu.position}
+                onChange={(e) =>
+                    setAddingMenu({ ...addingMenu, position: e.target.value })
+                }
+            />
+            <div className="addMenuModalBtns">
+                <button
+                    type="button"
+                    class="btn btn-success"
+                    onClick={() => addMenu()}
+                >
+                    Save
+                </button>
+                <button
+                    type="button"
+                    class="btn btn-secondary"
+                    onClick={() => setModal(!modal)}
+                >
+                    Close
+                </button>
+            </div>
+        </div>
+    );
 }
--- src/pages/admin.menu.js
+++ src/pages/admin.menu.js
@@ -3,118 +3,174 @@ import AddMenuModal from "../components/addMenuModal";
 import axios from "axios";
 
 export default function AdminMenu() {
-  const menuInit = {
-    menuName: "",
-    link: "",
-    position: "",
-  };
+    const menuInit = {
+        id: "",
+        menuName: "",
+        link: "",
+        position: "",
+    };
 
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [modal, setModal] = useState(false);
-  const [addingMenu, setAddingMenu] = useState(menuInit);
+    let arrWillDelete = [];
 
-  useEffect(() => {
-    getMenuData();
-  }, []);
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [modal, setModal] = useState(false);
+    const [addingMenu, setAddingMenu] = useState(menuInit);
 
-  function getMenuData() {
-    fetch("http://localhost:8000/api/menu")
-      .then((response) => response.json())
-      .then((dt) => {
-        setData(dt.result);
-      })
-      .catch((err) => setError(err))
-      .finally(() => setLoading(false));
-  }
+    useEffect(() => {
+        getMenuData();
+    }, []);
 
-  function deleteMenuData(id) {
-    axios
-      .delete(`http://localhost:8000/api/menu/${id}`)
-      .then((res) => {
-        setData(res.data.result);
-      })
-      .catch((err) => console.log(err));
-  }
+    function getMenuData() {
+        fetch("http://localhost:8000/api/menu")
+            .then((response) => response.json())
+            .then((dt) => {
+                setData(dt.result);
+            })
+            .catch((err) => setError(err))
+            .finally(() => setLoading(false));
+    }
 
-  function addMenu() {
-    axios
-      .post("http://localhost:8000/api/menu", addingMenu)
-      .then((res) => {
-        console.log(res);
-        setData(res.data.result);
-      })
-      .catch((err) => console.log(err));
+    function deleteMenuData(id) {
+        axios
+            .delete(`http://localhost:8000/api/menu/${id}`)
+            .then((res) => {
+                setData(res.data.result);
+            })
+            .catch((err) => console.log(err));
+    }
 
-    setModal(!modal);
-    console.log(addingMenu);
-    setAddingMenu(menuInit);
-  }
+    function addMenu() {
+        if (addingMenu.id) {
+            axios
+                .patch("http://localhost:8000/api/menu", addingMenu)
+                .then((res) => {
+                    setData(res.data.result);
+                })
+                .catch((err) => console.log(err));
+        } else {
+            axios
+                .post("http://localhost:8000/api/menu", addingMenu)
+                .then((res) => {
+                    console.log(res);
+                    setData(res.data.result);
+                })
+                .catch((err) => console.log(err));
+        }
 
-  return (
-    <div className="p-5">
-      <AddMenuModal
-        modal={modal}
-        setModal={setModal}
-        addMenu={addMenu}
-        addingMenu={addingMenu}
-        setAddingMenu={setAddingMenu}
-      />
-      <div className="row d-flex justify-content-center">
-        <div className="col-1">
-          <button
-            type="button"
-            class="btn btn-success"
-            onClick={() => setModal(!modal)}
-          >
-            Add Menu
-          </button>
+        setModal(!modal);
+        setAddingMenu(menuInit);
+    }
+
+    function startEditting(para) {
+        setAddingMenu(para);
+        setModal(!modal);
+    }
+
+    function CollectDataByGroup(para) {
+        if (arrWillDelete.includes(para)) {
+            arrWillDelete.splice(arrWillDelete.indexOf(para), 1);
+            console.log(arrWillDelete);
+        } else {
+            arrWillDelete.push(para);
+            console.log(arrWillDelete);
+        }
+    }
+
+    function DeleteByGroup() {
+        console.log("daragdaad bn shde ");
+        axios
+            .delete("http://localhost:8000/api/menu", {
+                body: "dhusafhdj",
+            })
+            .then((res) => {
+                setData(res.data.result);
+            })
+            .catch((err) => console.log(err));
+    }
+
+    return (
+        <div className="p-5">
+            <AddMenuModal
+                modal={modal}
+                setModal={setModal}
+                addMenu={addMenu}
+                addingMenu={addingMenu}
+                setAddingMenu={setAddingMenu}
+            />
+            <div className="row d-flex justify-content-center">
+                <div className="col-1">
+                    <button
+                        type="button"
+                        class="btn btn-success"
+                        onClick={() => setModal(!modal)}
+                    >
+                        Add Menu
+                    </button>
+                </div>
+                <div className="col-2">
+                    <button
+                        type="button"
+                        class="btn btn-danger"
+                        onClick={() => DeleteByGroup()}
+                    >
+                        Delete all selected menus
+                    </button>
+                </div>
+            </div>
+            <table className="table m-5">
+                <thead>
+                    <tr>
+                        <th scope="col">Select</th>
+                        <th scope="col">#</th>
+                        <th scope="col">Menu Name</th>
+                        <th scope="col">Menu Link</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((e, index) => {
+                        if (error) {
+                            return <div>Error</div>;
+                        } else if (loading) {
+                            return <div>Loading</div>;
+                        }
+                        return (
+                            <tr>
+                                <td>
+                                    <input
+                                        type="checkbox"
+                                        onChange={() =>
+                                            CollectDataByGroup(e.id)
+                                        }
+                                    />
+                                </td>
+
+                                <td>{index + 1}</td>
+                                <td>{e.menuName}</td>
+                                <td>{e.link}</td>
+                                <td>
+                                    <button
+                                        type="button"
+                                        class="btn btn-danger"
+                                        onClick={() => deleteMenuData(e.id)}
+                                    >
+                                        Delete
+                                    </button>
+                                    <button
+                                        type="button"
+                                        class="btn btn-warning"
+                                        onClick={() => startEditting(e)}
+                                    >
+                                        Edit
+                                    </button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
-        <div className="col-2">
-          <button type="button" class="btn btn-danger">
-            Delete all selected menus
-          </button>
-        </div>
-      </div>
-      <table className="table m-5">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Menu Name</th>
-            <th scope="col">Menu Link</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((e, index) => {
-            if (error) {
-              return <div>Error</div>;
-            } else if (loading) {
-              return <div>Loading</div>;
-            }
-            return (
-              <tr>
-                <th scope="row">{index + 1}</th>
-                <td>{e.menuName}</td>
-                <td>{e.link}</td>
-                <td>
-                  <button
-                    type="button"
-                    class="btn btn-danger"
-                    onClick={() => deleteMenuData(e.id)}
-                  >
-                    Delete
-                  </button>
-                  <button type="button" class="btn btn-warning">
-                    Edit
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+    );
 }
