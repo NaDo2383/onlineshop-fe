34ef7cd07da2df50b90d381431579db1ed424c74
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-02-07 08:04:48 +0800

    20230207

--- src/pages/admin.cate.js
+++ src/pages/admin.cate.js
@@ -2,77 +2,83 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 export default function AdminCate() {
-  const cateInit = {
-    id: "",
-    cateName: "",
-  };
+    const cateInit = {
+        id: "",
+        cateName: "",
+    };
 
-  const [cateData, setCateData] = useState([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [modal, setModal] = useState(false);
-  const [addingCate, setAddingCate] = useState(cateInit);
+    const [cateData, setCateData] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [modal, setModal] = useState(false);
+    const [addingCate, setAddingCate] = useState(cateInit);
 
-  useEffect(() => {
-    getCatData();
-  }, []);
+    useEffect(() => {
+        getCatData();
+    }, []);
 
-  function getCatData() {
-    axios
-      .get("http://localhost:8000/api/cate")
-      .then((res) => {
-        setCateData(res.data.result);
-      })
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
-  }
+    function getCatData() {
+        axios
+            .get("http://localhost:8000/api/cate")
+            .then((res) => {
+                setCateData(res.data.result);
+            })
+            .catch((err) => console.log(err))
+            .finally(() => setLoading(false));
+    }
 
-  return (
-    <div>
-      <div className="row d-flex justify-content-center">
-        <div className="col-1">
-          <button class="btn btn-success">Add new category</button>
+    return (
+        <div>
+            <div className="row d-flex justify-content-center">
+                <div className="col-1">
+                    <button class="btn btn-success">Add new category</button>
+                </div>
+                <div className="col-1">
+                    <button class="btn btn-danger">Delete</button>
+                </div>
+            </div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Select</th>
+                        <th>#</th>
+                        <th>Category Name</th>
+                        <th>Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cateData.map((e, index) => {
+                        if (error) {
+                            return <div>{error}</div>;
+                        } else if (loading) {
+                            return <div>Loading</div>;
+                        }
+                        return (
+                            <tr>
+                                <td>
+                                    <input type="checkbox" />
+                                </td>
+                                <td>{index + 1}</td>
+                                <td>{e.cateName}</td>
+                                <td>
+                                    <button
+                                        type="button"
+                                        class="btn btn-danger"
+                                    >
+                                        Delete
+                                    </button>
+                                    <button
+                                        type="button"
+                                        class="btn btn-warning"
+                                    >
+                                        Edit
+                                    </button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
-        <div className="col-1">
-          <button class="btn btn-danger">Delete</button>
-        </div>
-      </div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Select</th>
-            <th>#</th>
-            <th>Category Name</th>
-            <th>Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cateData.map((e, index) => {
-            if (error) {
-              return <div>{error}</div>;
-            } else if (loading) {
-              return <div>Loading</div>;
-            }
-            return (
-              <tr>
-                <td>
-                  <input type="checkbox" />
-                </td>
-                <td>{index + 1}</td>
-                <td>{e.cateName}</td>
-                <td>
-                  <button type="button" class="btn btn-danger">
-                    Delete
-                  </button>
-                  <button type="button" class="btn btn-warning">
-                    Edit
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+    );
 }
--- src/pages/admin.products.js
+++ src/pages/admin.products.js
@@ -1,84 +1,219 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function AdminProducts() {
-  const productInit = {
-    productName: "",
-    categoryId: "",
-    price: "",
-    thumb: "",
-  };
-  let [productItem, setProductItem] = useState(productInit);
-  return (
-    <div>
-      <div id="addMenuModal" className="d-flex flex-column">
-        <input
-          class="form-control addMenuModalInput"
-          type="text"
-          placeholder="Product name"
-          aria-label="default input example"
-          value={productItem.productName}
-          onChange={(e) => {
-            setProductItem({ ...productItem, productName: e.target.value });
-          }}
-        />
-        <input
-          class="form-control addMenuModalInput"
-          type="text"
-          placeholder="Price"
-          aria-label="default input example"
-          value={productItem.price}
-          onChange={(e) => {
-            setProductItem({ ...productItem, price: e.target.value });
-          }}
-        />
-        <input
-          class="form-control addMenuModalInput"
-          type="text"
-          placeholder="Price"
-          aria-label="default input example"
-          value={productItem.price}
-          onChange={(e) => {
-            setProductItem({ ...productItem, price: e.target.value });
-          }}
-        />
-        <input
-          class="form-control addMenuModalInput"
-          type="file"
-          placeholder="Position"
-          aria-label="default input example"
-          onChange={(e) => {
-            const url = "https://api.cloudinary.com/v1_1/dpgnl2yyr/upload";
+    const productInit = {
+        productName: "",
+        categoryId: "",
+        price: "",
+        thumb: "",
+        quantity: "",
+        description: "",
+    };
+    const [productItem, setProductItem] = useState(productInit);
+    const [cateData, setCateData] = useState([]);
+    const [userData, setUserData] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [loadingPic, setLoadingPic] = useState();
+    const [addingProduct, setAddingProduct] = useState(productInit);
+    let file = [];
 
-            const formData = new FormData();
+    useEffect(() => {
+        getUserData();
+        getCatData();
+    }, []);
 
-            let file = e.target.files[0];
-            formData.append("file", file);
-            formData.append("api_key", "675484793394138");
-            formData.append("folder", "shop");
-            formData.append("upload_preset", "abvttugs");
+    function getUserData() {
+        axios
+            .get("http://localhost:8000/api/user")
+            .then((res) => {
+                setUserData(res.data.result);
+            })
+            .catch((err) => console.log(err))
+            .finally(() => setLoading(false));
+    }
 
-            axios
-              .post(url, formData)
-              .then((res) => {
-                console.log(res);
-                setProductItem({ ...productItem, thumb: res.data.secure_url });
-              })
-              .catch((err) => alert(err))
-              .finally((res) => {
-                alert("upload finished");
-              });
-          }}
-        />
-        <div className="d-flex">
-          <button type="button" class="btn btn-success">
-            Save
-          </button>
-          <button type="button" class="btn btn-secondary">
-            Close
-          </button>
+    function getCatData() {
+        axios
+            .get("http://localhost:8000/api/cate")
+            .then((res) => {
+                setCateData(res.data.result);
+            })
+            .catch((err) => console.log(err))
+            .finally(() => setLoading(false));
+    }
+
+    async function sendPics(fieldName, files) {
+        setLoadingPic(true);
+
+        const url = "https://api.cloudinary.com/v1_1/dpgnl2yyr/upload";
+        const newArr = [];
+        for (let i = 0; i < files[0].length; i++) {
+            newArr.push(files[0][i]);
+        }
+
+        const promise = await Promise.all(
+            newArr.map((file) => {
+                const formData = new FormData();
+                formData.append("file", file);
+                formData.append("api_key", "675484793394138");
+                formData.append("folder", "shop");
+                formData.append("upload_preset", "abvttugs");
+
+                return axios.post(url, formData);
+            })
+        );
+
+        console.log(promise);
+
+        const arr = [];
+
+        promise.map((e) => {
+            arr.push(e.data.secure_url);
+        });
+
+        if (fieldName == "images") {
+            setProductItem({
+                ...productItem,
+                images: arr,
+            });
+        } else {
+            setProductItem({
+                ...productItem,
+                thumbImage: arr[0],
+            });
+        }
+
+        setLoadingPic(false);
+    }
+
+    function addProduct() {
+        axios
+            .post("http://localhost:8000/api/products", addingProduct)
+            .then((res) => {
+                setProductItem(res.data.result);
+            })
+            .catch((err) => console.log(err));
+    }
+
+    return (
+        <div>
+            <div id="addMenuModal" className="d-flex flex-column">
+                <input
+                    class="form-control addMenuModalInput"
+                    type="text"
+                    placeholder="Product name"
+                    aria-label="default input example"
+                    value={addingProduct.productName}
+                    onChange={(e) =>
+                        setAddingProduct({
+                            ...addingProduct,
+                            productName: e.target.value,
+                        })
+                    }
+                />
+                <input
+                    class="form-control addMenuModalInput"
+                    type="text"
+                    placeholder="Price"
+                    aria-label="default input example"
+                    value={addingProduct.price}
+                    onChange={(e) =>
+                        setAddingProduct({
+                            ...addingProduct,
+                            price: e.target.value,
+                        })
+                    }
+                />
+                <input
+                    class="form-control addMenuModalInput"
+                    type="text"
+                    placeholder="Quantity"
+                    aria-label="default input example"
+                    value={addingProduct.quantity}
+                    onChange={(e) =>
+                        setAddingProduct({
+                            ...addingProduct,
+                            quantity: e.target.value,
+                        })
+                    }
+                />
+                <textarea
+                    class="form-control addMenuModalInput"
+                    placeholder="Description"
+                    aria-label="default input example"
+                    value={addingProduct.description}
+                    onChange={(e) =>
+                        setAddingProduct({
+                            ...addingProduct,
+                            description: e.target.value,
+                        })
+                    }
+                />
+                <select className="form-select addMenuModalInput">
+                    <option>Choose user...</option>;
+                    {userData.map((e) => {
+                        return <option>{e.userName}</option>;
+                    })}
+                </select>
+                <div className="addMenuModalCheckbox row">
+                    {cateData.map((e) => {
+                        return (
+                            <div className="d-flex gap-2 modal-checkboxes">
+                                <input
+                                    type="checkbox"
+                                    className="form-control form-check-input"
+                                    id={e.id}
+                                />
+                                <label for={e.id}>{e.cateName}</label>
+                            </div>
+                        );
+                    })}
+                </div>
+                <input
+                    class="form-control addMenuModalInput"
+                    type="file"
+                    placeholder="Position"
+                    aria-label="default input example"
+                    onChange={(e) => {
+                        const arr = [];
+                        arr.push(e.target.files);
+                        sendPics("thumbImage", arr);
+                    }}
+                />
+                <input
+                    class="form-control addMenuModalInput"
+                    type="file"
+                    placeholder="Position"
+                    aria-label="default input example"
+                    onChange={(e) => {
+                        const arr = [];
+                        arr.push(e.target.files);
+                        sendPics("images", arr);
+                    }}
+                    multiple
+                />
+                {loadingPic ? (
+                    <div className="addMenuModalInput">Uploading pic ...</div>
+                ) : (
+                    ""
+                )}
+                <div className="d-flex addMenuModalInput gap-5">
+                    <button
+                        type="button"
+                        class="btn btn-success"
+                        onClick={() => {
+                            addProduct();
+                        }}
+                    >
+                        Save
+                    </button>
+                    <button type="button" class="btn btn-secondary">
+                        Close
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
